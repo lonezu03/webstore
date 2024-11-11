ec0c83b55af0fed9f3f3e9c4ec9c110f74566aac
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2024-11-11 21:21:57 +0700

    Revert "Api Respone Nếu ko trả về thì sửa tiếp version 2"

--- WebStore/WebStore/Service/IService/IUserService.cs
+++ WebStore/WebStore/Service/IService/IUserService.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebStore.Models;
 
 namespace WebStore.Service.IService
 {
     public interface IUserService
     {
-        Task<ApiResponse<string>> Authenticate(string username, string password);
+        Task<string> Authenticate(string username, string password);
 
     }
 }
